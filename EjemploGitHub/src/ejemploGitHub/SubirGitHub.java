60c0a9e6b0d521cd321ccd39cbaae5f36fbc19d6
ramonlau <44288234+ramonlau@users.noreply.github.com>
2018-10-19 12:42:05 +0200

    segunda linea

--- EjemploGitHub/src/ejemploGitHub/SubirGitHub.java
+++ EjemploGitHub/src/ejemploGitHub/SubirGitHub.java
@@ -5,5 +5,6 @@ public class SubirGitHub {
 	public static void main(String[] args) {
 		System.out.println("Ejemplo para subir proyecto a GitHub");
 		System.out.println("Esta es una nueva línea de código");
+		System.out.println("Segunda una nueva línea de código");
 	}
 }
